42fc57f499ad03e8b58dc33e633876d5e558257a
marfra@microsoft.com <marfra@microsoft.com>
2025-03-27 10:31:31 -0400

    Adding permissions for data contributor at the resource group level for telemetry and evaluations

---
+++ permissions.sh
@@ -0,0 +1,2 @@
+az role assignment create --role "Storage Blob Data Contributor" --scope /subscriptions/28d10200-70b0-476c-b004-c6ae29265897/resourceGroups/rg-dev-scm_ai --assignee-principal-type User --assignee-object-id "77af0205-9c85-413a-ab32-93dcce629a73"
+
